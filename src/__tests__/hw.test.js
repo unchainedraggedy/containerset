48eec366bf3ba27fb488355b0222a0a9ef23afe1
Ash <redcoolike@gmail.com>
2024-11-13 16:46:28 +0100

    testing

--- src/__tests__/hw.test.js
+++ src/__tests__/hw.test.js
@@ -10,3 +10,15 @@ test('add a member in team', () => {
     team.add("oleg")
     expect(team.members.has("oleg")).toBe(true)
 })
+
+test('checking adding same member', () => {
+    const team = new Team();
+    team.addAll(['oleg', 'oleg', 'oleg'])
+    expect(team.members.size).toBe(1)
+})
+
+test('returning array of different members', () => {
+    const team = new Team();
+    team.addAll(['oleg', 'kseniya', 'sasha'])
+    expect(team.toArray()).toEqual(['oleg', 'kseniya', 'sasha'])
+})
